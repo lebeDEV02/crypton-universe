bf0a362f4021a949e01905a7a5b810945c16ebcd
ledeDEV02 <vladimirlebedev00010@gmail.com>
2022-11-24 15:42:05 +0300

    added comic card create endpoint

--- server/controllers/comicCardController.js
+++ server/controllers/comicCardController.js
@@ -1,6 +1,24 @@
+const { ComicCard } = require('../models/models');
+const uuid = require('uuid');
+const path = require('path');
+const ApiError = require('../error/ApiError');
 class ComicCardController {
-	async create(req, res) {}
-	async getAll(req, res) {}
+	async create(req, res, next) {
+		try {
+			const { price, title } = req.body;
+			const { img } = req.files;
+			let fileName = uuid.v4() + '.jpg';
+			img.mv(path.resolve(__dirname, '..', 'static', fileName));
+			const comicCard = await ComicCard.create({ price, title, img: fileName });
+			return res.json(comicCard);
+		} catch (error) {
+			next(ApiError.badRequest(error));
+		}
+	}
+	async getAll(req, res) {
+		const comicCards = await ComicCard.findAll();
+		return res.json(comicCards);
+	}
 	async getOne(req, res) {}
 }
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const sequelize = require('./db');
 const app = express();
+const path = require('path');
 const router = require('./routes/index');
 const models = require('./models/models');
 const PORT = process.env.PORT || 8080;
@@ -10,6 +12,8 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.use('/api', router);
 app.use(errorHandler);
 const start = async () => {
