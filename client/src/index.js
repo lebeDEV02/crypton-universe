a697e059542dc7a4c235b3de7811ba8f6b2ecc6a
ledeDEV02 <vladimirlebedev00010@gmail.com>
2022-11-24 21:06:21 +0300

    moved client part of application to client directory

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './components/Login/Login';
-import { createBrowserRouter, RouterProvider, Route, BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
 	<BrowserRouter>
 		<App />
