785d555d69bf04470fe255323f2f0ddb3dde0bcb
ledeDEV02 <vladimirlebedev00010@gmail.com>
2022-11-24 08:17:45 +0300

    added middleware in order to catch server errors

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ const sequelize = require('./db');
 const app = express();
 const router = require('./routes/index');
 const PORT = process.env.PORT || 8080;
+const errorHandler = require('../middleware/ErrorHandlingMiddleware');
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+app.use(errorHandler());
 const start = async () => {
 	try {
 		await sequelize.authenticate();
